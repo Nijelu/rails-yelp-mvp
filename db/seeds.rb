eb238c92fc5df1971fbbc29b5d105530f0be2230
Julien Ducrocq <julien.ducrocq77@gmail.com>
2018-08-15 11:58:45 +0200

    Exo 05-03-01 Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(
+  name: 'Hello',
+  address: 'ici',
+  phone_number: '0213548789',
+  category: 'chinese'
+)
+
+Restaurant.create(
+  name: 'World',
+  address: 'ailleurs',
+  phone_number: '0313548789',
+  category: 'italian'
+)
+
+Restaurant.create(
+  name: 'Salut',
+  address: 'nulle part',
+  phone_number: '0413548789',
+  category: 'japanese'
+)
+
+Restaurant.create(
+  name: 'Secret Dish',
+  address: 'là',
+  phone_number: '0513548789',
+  category: 'french'
+)
+
+Restaurant.create(
+  name: 'Snap',
+  address: 'là-bas',
+  phone_number: '0613548789',
+  category: 'belgian'
+)
